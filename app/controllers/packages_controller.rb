130fcb0bae9e1ce8ad7836bc8b89164338419648
DanielLChang <daniellawrencechang@gmail.com>
2017-01-26 22:24:23 -0800

    Rough package model and controller

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,2 +1,30 @@
 class PackagesController < ApplicationController
+
+  def create
+    @package = Package.find_by(package_params)
+
+    if @package
+      render json: { error: "Package already tracked" }
+    else
+      @package = Package.new(package_params)
+      render json: { package_id: @package.id }
+    end
+  end
+
+  def update
+    @package = Package.find(params[:id])
+
+    if @package
+    else
+      render json: { error: "Can not find package" }
+    end
+
+  end
+
+  private
+
+  def package_params
+    params.require(:package).permit(:tracking_number, :phone_number, :realtime_updates)
+  end
+
 end
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,6 @@
 class Package < ApplicationRecord
+
+  validates :phone_number, presence: true, length: { is: 10 }
+  validates :tracking_number, presence: true
+
 end
