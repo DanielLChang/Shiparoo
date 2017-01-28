87ec8651133c75e5e3cc30add8abe946ec9624ab
DanielLChang <daniellawrencechang@gmail.com>
2017-01-28 14:26:01 -0800

    Write methods in package for generate pin and send pin

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,6 +1,18 @@
+require 'twilio-ruby'
+
 class Package < ApplicationRecord
 
   validates :phone_number, presence: true, length: { is: 10 }
   validates :tracking_number, presence: true
+  validates :pin, presence: true, length: { is: 4 }
+
+  def generate_pin
+    self.pin = rand(0..9999).to_s.rjust(4, "0")
+    self.save
+  end
+
+  def send_pin
+    TwilioClient.instance.send_pin(self.phone_number, self.pin)
+  end
 
 end
