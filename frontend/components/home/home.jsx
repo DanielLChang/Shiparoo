06225069695745585036a3e3070b14ab0c9358cd
DanielLChang <daniellawrencechang@gmail.com>
2017-01-30 21:10:26 -0800

    Form for inputting package details

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,11 +2,53 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tracking_number: "",
+      carrier: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  update(field) {
+    return (e) => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    debugger;
+  }
+
   render() {
     return (
-      <div className="home-container">
+      <form className="home-container"
+            onSubmit={ this.handleSubmit }>
         <h2>Home</h2>
-      </div>
+
+        <h3>Tracking Number</h3>
+        <input
+          type="text"
+          placeholder="tracking number"
+          onChange={ this.update("tracking_number") }>
+        </input>
+
+        <select onChange={ this.update("carrier") }>
+          <option value="UPS">UPS</option>
+          <option value="USPS">USPS</option>
+          <option value="Parcelforce">Parcelforce</option>
+          <option value="Deutsche Post">Deutsche Post</option>
+          <option value="UberRUSH">UberRUSH</option>
+          <option value="DHL Express">DHL Express</option>
+        </select>
+
+        <input type="submit" value="Start Tracking"></input>
+      </form>
     );
   }
 }
--- frontend/shiparoo.jsx
+++ frontend/shiparoo.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-import Root from './components/root';
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<Root />, root);
