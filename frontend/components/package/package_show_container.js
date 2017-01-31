78bc5b493f99d83dd53199bbcb61dafff690c819
Calvin Yau <calvinj.yau@gmail.com>
2017-01-31 10:11:54 -0800

    Add package show component start

---
+++ frontend/components/package/package_show_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+
+import PackageShow from './package_show';
+
+const mapStateToProps = () => {
+
+};
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PackageShow);
