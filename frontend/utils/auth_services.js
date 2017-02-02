210c293317ce38dfe07cd78b6437fe901528652e
Justin Suen <jsuen27@gmail.com>
2017-02-01 19:43:38 -0800

    I HATE AUTH0

--- frontend/utils/auth_services.js
+++ frontend/utils/auth_services.js
@@ -5,25 +5,21 @@ import { browserHistory } from 'react-router';
 export default class AuthService extends EventEmitter {
   constructor() {
     super();
-      // Configure Auth0
+    
     this.lock = new Auth0Lock('HQyc8BbQc47Drpa85hJca6t6THDNOAXg', 'justinsuen.auth0.com', {
       auth: {
         redirectUrl: 'http://localhost:3000/home',
         responseType: 'token',
       },
     });
-    // Add callback for lock `authenticated` event
+
     this.lock.on('authenticated', this._doAuthentication.bind(this));
-    // binds login functions to keep this context
     this.login = this.login.bind(this);
   }
 
   _doAuthentication(authResult) {
-    // Saves the user token
     this.setToken(authResult.idToken);
-    // navigate to the home route
     browserHistory.replace('/home');
-    // Async loads user profile data
     this.lock.getProfile(authResult.idToken, (error, profile) => {
       if (error) {
         console.log('Error loading the Profile', error);
@@ -34,46 +30,37 @@ export default class AuthService extends EventEmitter {
   }
 
   login() {
-    // Call the show method to display the widget.
     this.lock.show();
   }
 
   loggedIn() {
-    // Checks if there is a saved token and it's still valid
     return !!this.getToken();
   }
 
   setProfile(profile) {
-    // Saves profile data to local storage
     localStorage.setItem('profile', JSON.stringify(profile));
-    // Triggers profile_updated event to update the UI
     this.emit('profile_updated', profile);
   }
 
   getProfile() {
-    // Retrieves the profile data from local storage
     const profile = localStorage.getItem('profile');
     return profile ? JSON.parse(localStorage.profile) : {};
   }
 
   logout() {
-    // Clear user token and profile data from local storage
     localStorage.removeItem('id_token');
     localStorage.removeItem('profile');
   }
 
   setToken(idToken) {
-    // Saves user token to local storage
     localStorage.setItem('id_token', idToken);
   }
 
   getToken() {
-    // Retrieves the user token from local storage
     return localStorage.getItem('id_token');
   }
 
   logout() {
-    // Clear user token from local storage
     localStorage.removeItem('id_token');
   }
 }
