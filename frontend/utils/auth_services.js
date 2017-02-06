b689b25f7058554f64cf58412860c2b06aa2f5c2
Justin Suen <jsuen27@gmail.com>
2017-02-06 14:20:06 -0800

    Custom url

--- frontend/utils/auth_services.js
+++ frontend/utils/auth_services.js
@@ -8,7 +8,7 @@ export default class AuthService extends EventEmitter {
     super();
 
     const devUrl = 'http://localhost:3000/home';
-    const prodUrl = 'http://shiparoo.herokuapp.com';
+    const prodUrl = 'http://www.shiparoo.us';
 
     this.lock = new Auth0Lock('HQyc8BbQc47Drpa85hJca6t6THDNOAXg', 'justinsuen.auth0.com', {
       auth: {
